1ebb074d18f30d162a7140cb569a37e73a8e74d1
kayacekovic <kayacekovic@gmail.com>
2021-01-21 16:12:12 +0300

    multiselect and belongsToMany compatibility

--- src/DynamicSelect.php
+++ src/DynamicSelect.php
@@ -12,12 +12,74 @@ class DynamicSelect extends Field
     use DependsOnAnotherField;
 
     public $component = 'dynamic-select';
+    public $labelKey;
+    public $multiselect = false;
 
     public function resolve($resource, $attribute = null)
     {
         $this->extractDependentValues($resource);
 
         return parent::resolve($resource, $attribute);
+    }
+
+    /**
+     * Makes the field to manage a BelongsToMany relationship.
+     * todo: dependsOn
+     *
+     * @param string $resourceClass The Nova Resource class for the other model.
+     * @return DynamicSelect
+     **/
+    public function belongsToMany($resourceClass)
+    {
+        $model = $resourceClass::$model;
+        $primaryKey = (new $model)->getKeyName();
+
+        $this->resolveUsing(function ($value) use ($primaryKey, $resourceClass) {
+            $value = collect($value)->map(function ($option) use ($primaryKey) {
+                return [
+                    'label' => $option->{$this->labelKey ?: 'name'},
+                    'value' => $option->{$primaryKey},
+                ];
+            });
+
+            return $value;
+        });
+
+        $this->fillUsing(function ($request, $model, $requestAttribute, $attribute) {
+            $model::saved(function ($model) use ($attribute, $request) {
+                // Validate
+                if (!method_exists($model, $attribute)) {
+                    throw new RuntimeException("{$model}::{$attribute} must be a relation method.");
+                }
+
+                $relation = $model->{$attribute}();
+
+                if (!method_exists($relation, 'sync')) {
+                    throw new RuntimeException("{$model}::{$attribute} does not appear to model a BelongsToMany or MorphsToMany.");
+                }
+
+                // Sync
+                $relation->sync($request->get($attribute) ?? []);
+            });
+        });
+
+        $this->multiselect();
+
+        return $this;
+    }
+
+    public function multiselect($multiselect = true)
+    {
+        $this->multiselect = $multiselect;
+
+        return $this;
+    }
+
+    public function labelKey($labelKey)
+    {
+        $this->labelKey = $labelKey;
+
+        return $this;
     }
 
     public function meta()
@@ -30,7 +92,9 @@ class DynamicSelect extends Field
             'placeholder' => __('Pick a value'),
             'selectLabel' => __('Press enter to select'),
             'deselectLabel' => __('Press enter to remove'),
-            'selectedLabel' => __('Selected')
+            'selectedLabel' => __('Selected'),
+            'labelKey' => $this->labelKey,
+            'multiselect' => $this->multiselect,
         ], $this->meta);
     }
 }
